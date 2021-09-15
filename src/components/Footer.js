469ac5502cc6bfb7fada0b0d0b43b5388fcb74fe
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2021-09-15 00:06:16 -0300

    Footer working with history

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Footer() {
+    const history = useHistory();
 
-  // Get the history object
+    const goBack = () => {
+        history.goBack();
+    };
 
-  const goBack = () => {
-    // imperatively redirect back
+    const goForward = () => {
+        history.goForward();
+    };
 
-  }
-
-  const goForward = () => {
-    // imperatively redirect forward
-    
-  }
-
-  return (
-    <footer>
-      <button onClick={goBack}>Back</button>
-      <button onClick={goForward}>Forward</button>
-    </footer>
-  );
+    return (
+        <footer>
+            <button onClick={goBack}>Back</button>
+            <button onClick={goForward}>Forward</button>
+        </footer>
+    );
 }
