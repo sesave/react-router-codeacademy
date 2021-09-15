68100a15f8945990d26b1a132aeb4236b87750c5
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2021-09-15 00:08:34 -0300

    queryParams added to search bar

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -5,18 +5,17 @@ import {
     filterArticles,
 } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 // Import Link from React Router
 
 export default function Articles() {
     const articles = useSelector(selectArticles);
 
-    // grab the search value from useLocation()
+    const { search } = useLocation();
 
-    // get the queryParams from new URLSearchParams()
-
-    const title = ""; // <-- fix me!
+    const queryParams = new URLSearchParams(search);
+    const title = queryParams.get('title')
 
     const filteredArticles = title
         ? filterArticles(title, articles)
