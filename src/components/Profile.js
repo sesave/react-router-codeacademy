ff27b8077f4f1d9c850fe1498c732ff7bc513b27
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2021-09-15 00:01:39 -0300

    using useRouteMatch

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Route, Link } from "react-router-dom";
-import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
+import { Route, Link, useRouteMatch } from "react-router-dom";
+import {
+    selectCurrentUser,
+    selectIsLoggedIn,
+} from "../features/session/sessionSlice";
 import EditProfileForm from "./EditProfileForm";
 
-export default function Profile () {
-  const currentUser = useSelector(selectCurrentUser)
-  const loggedIn = useSelector(selectIsLoggedIn);
+export default function Profile() {
+    const currentUser = useSelector(selectCurrentUser);
+    const loggedIn = useSelector(selectIsLoggedIn);
 
-  // call useRouteMatch() to get the url and path
-  
-  // use loggedIn to return a Redirect
-  
-  return (
-    <main>
-      <h1>{currentUser.username}</h1>
-      <Link to={"/profile/edit"}>Edit</Link>
-      {/* Render a route for EditProfileForm */}
-    </main>
-  )
+    // call useRouteMatch() to get the url and path
+    const { url, path } = useRouteMatch();
+
+    // use loggedIn to return a Redirect
+
+    return (
+        <main>
+            <h1>{currentUser.username}</h1>
+            <Link to={url + "/edit"}>Edit</Link>
+            <Route path={path + "/edit"}>
+                <EditProfileForm />
+            </Route>
+        </main>
+    );
 }
